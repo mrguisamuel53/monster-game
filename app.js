cec624ac9e57f8e5c0f6174802238eb4644a4983
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-21 15:55:48 -0300

    Percentage added

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ window.onload = function() {
 		el: '#app',
 		data: {
 			startGame: true,
+			lifePlayer: 100,
+			lifeEnemy: 100
+		},
+		computed: {
+			/*
+			(event) {
+				this.lifePlayer = 100
+				this.lifeEnemy = 100
+			}
+			*/
+		},
+		methods: {
+			damage(event, min, max) {
+				this.lifePlayer -= (Math.floor(Math.random() * max+2) + min+2)
+				this.lifeEnemy -= (Math.floor(Math.random() * max) + min)
+			}
 		}
 	});
 }
