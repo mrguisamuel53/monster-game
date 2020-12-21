cf734c52497c4886c2f8d8fc162763fce89f99cf
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-21 18:34:05 -0300

    Special attack finished

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ window.onload = function() {
 			}
 		},
 		methods: {
-			damage(event, min, max) {
-				this.lifePlayer -= (Math.floor(Math.random() * max+2) + min+2)
-				this.lifeEnemy -= (Math.floor(Math.random() * max) + min)
+			damage(event, min, max, add1, add2, add3, add4) {
+				this.lifePlayer -= (Math.floor(Math.random() * max+add1) + min+add2)
+				this.lifeEnemy -= (Math.floor(Math.random() * max+add3) + min+add4)
 				if(this.lifePlayer <= 0) this.lifePlayer = 0
 				if(this.lifeEnemy <= 0) this.lifeEnemy = 0
 			},
