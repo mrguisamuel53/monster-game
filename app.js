6be70cfe9967908c3e5675f98546974594f4c32e
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-22 00:06:09 -0300

    Heal button finished and reset game to initial values on button

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ window.onload = function() {
 		el: '#app',
 		data: {
 			startGame: true,
-			lifePlayer: 100,
-			lifeEnemy: 100
-		},
-		computed: {
-			resetGame: function() {
-				this.lifePlayer = 100
-				this.lifeEnemy = 100
-			}
+			lifePlayer: 100.0,
+			lifeEnemy: 100.0,
+			healPlayer: 0.0,
+			attackEnemy: 0.0
 		},
 		methods: {
 			damage(event, min, max, add1, add2, add3, add4) {
@@ -19,6 +15,19 @@ window.onload = function() {
 				if(this.lifePlayer <= 0) this.lifePlayer = 0
 				if(this.lifeEnemy <= 0) this.lifeEnemy = 0
 			},
+			heal(event, min, max, hth, atk) {
+				hth = Math.floor(Math.random() * max) + min
+				atk = Math.floor(Math.random() * max-3) + min-3
+				this.lifePlayer += (hth - atk)
+				if(this.lifePlayer >= 100) this.lifePlayer = 100
+				if(this.lifePlayer <= 0) this.lifePlayer = 0
+			},
+			resetGame() {
+				this.startGame = !this.startGame
+				this.lifePlayer = 100
+				this.lifeEnemy = 100
+			}
+
 		}
 	});
 }
