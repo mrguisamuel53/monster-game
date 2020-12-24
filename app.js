2940ff232f260f54a552e312e30b1d882d3ac24b
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-24 00:07:45 -0300

    Logic of saves values: attack and heal by logs in screen

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ window.onload = function() {
 			gameMode: true,
 			lifePlayer: 100.0,
 			lifeEnemy: 100.0,
-			healPlayer: 0.0,
-			attackEnemy: 0.0,
 			message: 0,
+			playerActions: [],
+			enemyActions: [],
+			counter: -1,
+		},
+		computed: {
+			returnPlayer() {
+				return this.playerActions[this.counter]
+			},
+			returnEnemy() {
+				return this.enemyActions[this.counter]
+			}
 		},
 		methods: {
 			death() {
@@ -15,19 +24,33 @@ window.onload = function() {
 				else if(this.lifeEnemy == 0 && this.lifePlayer != 0) this.message = 2
 				else if(this.lifeEnemy == 0 && this.lifePlayer == 0) this.message = 3
 			},
-			damage(event, min, max, add1, add2, add3, add4) {
-				this.lifePlayer -= (Math.floor(Math.random() * max+add1) + min+add2)
-				this.lifeEnemy -= (Math.floor(Math.random() * max+add3) + min+add4)
+			damage(event, min, max, atk1, atk2) {
+				this.counter++
+				atk1 = Math.floor(Math.random() * max+atk1) + min
+				atk2 = Math.floor(Math.random() * max+atk2) + min
+				
+				this.playerActions.push(atk1)
+				this.enemyActions.push(atk2)
+				
+				//this.counter++
+				console.log(this.playerActions[this.counter])
+				//console.log(test)
+				
+				this.lifePlayer -= atk1
+				this.lifeEnemy -= atk2
 				if(this.lifePlayer <= 0 || this.lifeEnemy <= 0) {
 					this.gameMode = true
 					if(this.lifePlayer <= 0) this.lifePlayer = 0
-					else if(this.lifeEnemy <= 0) this.lifeEnemy = 0
+					if(this.lifeEnemy <= 0) this.lifeEnemy = 0
 					this.death()
 				}
 			},
 			heal(event, min, max, hth, atk) {
+				this.counter++
 				hth = Math.floor(Math.random() * max) + min
 				atk = Math.floor(Math.random() * max-3) + min-3
+				this.playerActions.push(hth)
+				this.enemyActions.push(atk)
 				this.lifePlayer += (hth - atk)
 				if(this.lifePlayer >= 100) this.lifePlayer = 100
 				if(this.lifePlayer <= 0) {
