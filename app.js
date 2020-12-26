ab05aa3157d43359b3a223251f8da08d0fbe42a2
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-25 23:55:42 -0300

    System log

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ window.onload = function() {
 			message: 0,
 			playerActions: [],
 			enemyActions: [],
+			logMessages: [],
 			counter: -1,
+			select: 0,
+			setColor: ''
 		},
 		computed: {
 			returnPlayer() {
@@ -16,7 +19,7 @@ window.onload = function() {
 			},
 			returnEnemy() {
 				return this.enemyActions[this.counter]
-			}
+			},
 		},
 		methods: {
 			death() {
@@ -26,16 +29,17 @@ window.onload = function() {
 			},
 			damage(event, min, max, atk1, atk2) {
 				this.counter++
+				this.select = 1
+				//this.addMessage()
 				atk1 = Math.floor(Math.random() * max+atk1) + min
 				atk2 = Math.floor(Math.random() * max+atk2) + min
-				
+
 				this.playerActions.push(atk1)
 				this.enemyActions.push(atk2)
-				
-				//this.counter++
-				console.log(this.playerActions[this.counter])
-				//console.log(test)
-				
+
+				this.logMessages.push(`Enemy attacked with ${this.enemyActions[this.counter]} strength`, `Player attacked with ${this.playerActions[this.counter]} strength`)
+				console.log(this.setColo)
+
 				this.lifePlayer -= atk1
 				this.lifeEnemy -= atk2
 				if(this.lifePlayer <= 0 || this.lifeEnemy <= 0) {
@@ -44,26 +48,47 @@ window.onload = function() {
 					if(this.lifeEnemy <= 0) this.lifeEnemy = 0
 					this.death()
 				}
+				this.logMessages.reverse()
+				this.testString()
 			},
 			heal(event, min, max, hth, atk) {
 				this.counter++
+				this.select = 2
 				hth = Math.floor(Math.random() * max) + min
 				atk = Math.floor(Math.random() * max-3) + min-3
 				this.playerActions.push(hth)
 				this.enemyActions.push(atk)
 				this.lifePlayer += (hth - atk)
+
+				this.logMessages.push(`Enemy attacked with ${this.enemyActions[this.counter]} strength`, `Player healed with ${this.playerActions[this.counter]} strength`)
+				
+				console.log(this.logMessages)
 				if(this.lifePlayer >= 100) this.lifePlayer = 100
 				if(this.lifePlayer <= 0) {
 					this.gameMode = true
 					this.lifePlayer = 0
 					this.death()
 				}
+				this.logMessages.reverse()
 			},
 			resetGame() {
 				this.gameMode = !this.gameMode
 				this.lifePlayer = 100
 				this.lifeEnemy = 100
 				this.message = 0
+			},
+			testString() {
+				for(i = 0; i < this.logMessages.length; i++)
+				{
+					if(this.logMessages[i].includes('Player'))
+					{
+						this.setColor = 'blue'	
+					}
+					else if(this.logMessages[i].includes('Enemy'))
+					{
+						this.setColor = 'red'
+					}
+				}
 			}
 		}
 	});
