f3d1491a42d29d26492e1c8006de8ec310d40cc5
Guilherme Samuel <guisamuel800@gmail.com>
2020-12-27 17:00:28 -0300

    Attacking (Normal & Special)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
 	running: false,
@@ -15,9 +15,25 @@ var app = new Vue({
 		this.running = true
 		this.playerLife = 100
 		this.monsterLife = 100
+	},
+	attack(special) {
+		// console.log(special, this.getRandom(5, 10))
+		this.hurt('monsterLife', 7, 12, special)
+		this.hurt('playerLife', 7, 12, false)
+	},
+	hurt(prop, min, max, special) {
+		const plus = special ? 5 : 0
+		const hurt = this.getRandom(min + plus, max + plus)
+		this[prop] = Math.max(this[prop] - hurt, 0)
+	}, 
+	getRandom(min, max) {
+		const value = Math.random() * (max - min) + min
+		return Math.round(value)
 	}
-    },
+    },	
     watch: {
-        
+    	hasResult(value) {
+		if(value) this.running = false
+	}
     }
 });
